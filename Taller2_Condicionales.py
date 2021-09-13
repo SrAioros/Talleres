a95d50d12c6ca133172002e9657bf9f412f041c6
SrAioros <danny.vega96@gmail.com>
2021-09-12 20:07:35 -0500

    Ejercicio condicional 2

--- Taller2_Condicionales.py
+++ Taller2_Condicionales.py
@@ -12,19 +12,41 @@ cantidad = int(input('Digite cantidad de camisas a comprar: '))
 subtotal = precio * cantidad
 descuento = 0
 
-if(precio <= 0 or cantidad <= 0):
-    print('Los datos no pueden ser menor a 1')
+# Condicional para que los datos digitados sean válidos
+if(precio <= 0 or cantidad <= 0):  
+    print('Los datos no pueden ser menor o igual a 0')
+# Condicional para el descuento del 30% si la cantidad de camisas es igual o 
+# mayor a 3
 elif (cantidad >= 3):
     descuento = subtotal * 0.3
     total = subtotal - descuento
-    print(f"El total a pagar es: ${total}")
-    print(f"El descuento aplicado es: ${descuento}")
+    print(f'El total a pagar es: ${total}')
+    print(f'El descuento aplicado es: ${descuento}')
+# Condicional para el descuento del 10% si la cantidad de camisas es menor a 3
 else:
     descuento = subtotal * 0.1
     total = subtotal - descuento
-    print(f"El total a pagar es: ${total}")
-    print(f"El descuento aplicado es: ${descuento}")
+    print(f'El total a pagar es: ${total}')
+    print(f'El descuento aplicado es: ${descuento}')
 
+# 2.
+
+precio2 = float(input('Digite el total de la compra: $'))
+num_azar = int(input('Digite un número al azar mayor a 0: '))
+descuento2 = 0
+# Condicional para que los datos digitados sean válidos 
+if(precio2 <= 0 or num_azar <= 0):
+    print('Los datos no pueden ser menor o igual a 0')
+# Condicional para el descuento del 15% si el número al azar es menor a 75
+elif(num_azar <= 74):
+    descuento2 = precio2 * 0.15
+    total2 = precio2 - descuento2
+# Condicional para el descuento del 20% si el número al azar es mayor a 74
+else:
+    descuento2 = precio2 * 0.2
+    total2 = precio2 - descuento2
+    print(f"El total a pagar es: ${total2}")
+    print(f"El descuento aplicado es: ${descuento2}")
 
 
 
