52d58a572cc05e7e9e4ce7e5dde287a5b3e33fd7
SrAioros <danny.vega96@gmail.com>
2021-09-12 21:16:30 -0500

    Ejercicio condicional 4

--- Taller2_Condicionales.py
+++ Taller2_Condicionales.py
@@ -67,7 +67,39 @@ else:
     print(f'El porcentaje aplicado es: {intereses} ')
     print(f'El valor de la cuota es: ${cuota}')
 
+# 4. 
 
+puntaje_dia1 = float(input('Digite los puntos del primer día: '))
+puntaje_dia2 = float(input('Digite los puntos del segundo día: '))
+puntaje_dia3 = float(input('Digite los puntos del tercer día: '))
+puntaje_dia4 = float(input('Digite los punto del cuarto día: '))
+puntaje_dia5 = float(input('Digite los punto del quinto día: '))
+
+ganancias_dia1 = float(input('Digite las ganacias del primer día: '))
+ganancias_dia2 = float(input('Digite las ganacias segundo día: '))
+ganancias_dia3 = float(input('Digite las ganacias del tercer día: '))
+ganancias_dia4 = float(input('Digite las ganacias del cuarto día: '))
+ganancias_dia5 = float(input('Digite las ganacias del quinto día: '))
+
+promedio = (puntaje_dia1 + puntaje_dia2 + puntaje_dia3 + puntaje_dia4 + puntaje_dia5) / 5
+ganancias = (ganancias_dia1 + ganancias_dia2 + ganancias_dia3 + ganancias_dia4 + ganancias_dia5)
+multa = 0
+
+# Condicional para que los datos digitados sean válidos
+if(puntaje_dia1 < 0 or puntaje_dia2 < 0 or puntaje_dia3 < 0 
+   or puntaje_dia4 < 0 or puntaje_dia5 < 0
+   or ganancias_dia1 < 0 or ganancias_dia2 < 0 or ganancias_dia3 < 0
+   or ganancias_dia4 < 0 or ganancias_dia5 < 0):
+    print('Los datos no pueden ser menor a 0')
+# Condicional para aplicar multa
+elif(promedio > 170):
+    multa = ganancias * 0.5
+    print(f'El promedio de puntos es: {promedio}')
+    print(f'Se efectua la multa de: ${multa}')
+# Condicional para cuando el puntaje de contaminación es leve
+else:
+    print('Todo esta en orden y no aplica multa')
+ 
 
 
 
